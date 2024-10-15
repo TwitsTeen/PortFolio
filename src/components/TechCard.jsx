8cd3ecab82f917b18fe86e3ec723799a51ec2ee7
Fierquin Matteo <matteo_fierquin@ens.univ-artois.fr>
2024-10-15 09:45:36 +0200

    Changed some colors

--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const TechCard = ({ title, logo }) => {
   return (
-    <div className="flex flex-col items-center p-4 m-4 w-56 h-44 rounded-lg shadow-lg bg-base100 transform transition-transform duration-300 hover:scale-110">
+    <div className="flex flex-col items-center p-4 m-4 w-56 h-44 rounded-lg shadow-lg bg-base-300 transform transition-transform duration-300 hover:scale-110">
       <img src={logo} alt={title} width={100} className="my-auto" />
       <p className="text-xl font-semibold">{title}</p>
     </div>
